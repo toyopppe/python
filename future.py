e5a92ee7ded0c140035db771f2a43cab572263af
tatsuya toyoda <tatsuya toyoda>
2016-11-04 15:17:43 +0900

    add new file

---
+++ future.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+i = 3 / 2
+print(i, end='\n')
+print('だいこん')
